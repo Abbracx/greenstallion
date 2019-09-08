0f07417da8d7e5a3aba0bfe77062976b8483ffe0
KC <egbosikelechi@gmail.com>
2019-09-08 12:03:25 +0100

    setup static filesll

--- greenstallion/settings.py
+++ greenstallion/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
 os.path.join(BASE_DIR, 'assetss')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
