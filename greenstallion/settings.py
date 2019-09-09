a291ef5a1f836d3ae618710bf8414bf96c4d5fcb
KC <egbosikelechi@gmail.com>
2019-09-09 17:28:22 +0100

    hahah

--- greenstallion/settings.py
+++ greenstallion/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'assetss')
+os.path.join(BASE_DIR, 'assets')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
